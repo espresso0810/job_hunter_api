f99f36e7b585b8b163f56266a2135c64a13a9878
hungp03 <ph.hung0903@gmail.com>
2024-07-18 23:41:31 +0700

    formart api login

--- spring-jobhunter-api/src/main/java/vn/hp/jobhunter/controller/AuthController.java
+++ spring-jobhunter-api/src/main/java/vn/hp/jobhunter/controller/AuthController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.hp.jobhunter.domain.User;
 import vn.hp.jobhunter.domain.dto.LoginDTO;
-import vn.hp.jobhunter.domain.dto.ResponseLoginDTO;
+import vn.hp.jobhunter.domain.dto.ResLoginDTO;
+import vn.hp.jobhunter.service.UserService;
 import vn.hp.jobhunter.util.SecurityUtil;
 
 @RestController
@@ -19,22 +21,36 @@ import vn.hp.jobhunter.util.SecurityUtil;
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil){
+    private final UserService userService;
+
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil, UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
+
     @PostMapping("login")
-    public ResponseEntity<ResponseLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
         //Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
         //xác thực người dùng => cần viết hàm loadUserByUsername
+
         // cơ chế của authentication: không lưu mật khẩu nếu login thành công
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         // Tạo token
         String accessToken = this.securityUtil.createToken(authentication);
+
         // Nạp thông tin vào securityContext
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        ResponseLoginDTO responseLoginDTO = new ResponseLoginDTO();
+        ResLoginDTO responseLoginDTO = new ResLoginDTO();
+
+        // Lấy thông tin người dùng trong db thay vì thay đổi spring security
+        User currentUserDB = this.userService.getUserByUsername(loginDTO.getUsername());
+        if (currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(currentUserDB.getId(), currentUserDB.getEmail(), currentUserDB.getName());
+            responseLoginDTO.setUser(userLogin);
+        }
+        
         responseLoginDTO.setAccessToken(accessToken);
         return ResponseEntity.ok(responseLoginDTO);
     }
